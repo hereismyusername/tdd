ff0aaacbd1c78a5e396b5f8f189561d72c093819
hereismyusername <my.e.address123@gmail.com>
2017-09-05 14:05:30 +0000

    finish adding post to db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,15 @@ class PostsController < ApplicationController
   def index
 
   end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:message)
+  end
 end
