8a328b339e5c5c85950b72ad9b773734b2dbdeae
hereismyusername <my.e.address123@gmail.com>
2017-09-05 13:52:12 +0000

    add gram to db commit 2

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "posts#create action" do
+    it "should successfully create a new gram in our database" do
+      post :create, params: { post: { message: 'Hello!' } }
+      expect(response).to redirect_to root_path
+
+      post = Post.last
+      expect(post.message).to eq("Hello!")
+    end
+  end
 end
